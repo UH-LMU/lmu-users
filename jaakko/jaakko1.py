c97503d02a9001a3d63c8a257fbe74481031951f
Harri Jäälinoja <harri.jaalinoja at helsinki.fi>
2014-09-15 11:36:45 +0000

    find centers

--- jaakko/jaakko1.py
+++ jaakko/jaakko1.py
@@ -1,4 +1,4 @@
-from ij import IJ
+from ij import IJ, ImagePlus
 from ij.gui import Roi, ShapeRoi, PointRoi
 from ij.measure import Measurements, ResultsTable
 from ij.plugin.filter import ParticleAnalyzer as PA
@@ -6,10 +6,13 @@ from ij.plugin.filter import ThresholdToSelection
 from ij.plugin.frame import RoiManager
 from ij.gui import WaitForUserDialog as Wait
 
+from skeleton_analysis import *
+
 import glob
 import sys
 
 from java.lang import Double
+from java.awt import Color
 
 inputDir = "/input/LMU-active2/Harri/Data/Jaakko/sample 0001.tif_Files/"
 
@@ -20,43 +23,70 @@ nucleus = tmp[0]
 tmp = glob.glob(inputDir + "actin*")
 actin = tmp[0]
 
-imp_fn = IJ.openImage(fibronectin)
+# original images
+imp_fn_orig = IJ.openImage(fibronectin)
+imp_nuc_orig = IJ.openImage(nucleus)
+
+# work copies
+imp_fn = imp_fn_orig.duplicate()
+imp_nuc = imp_nuc_orig.duplicate()
 
 IJ.run(imp_fn,"Set Scale...", "distance=1 known=1 pixel=1 unit=pixels")
 IJ.run(imp_fn,"Gaussian Blur...","sigma=5")
 IJ.run(imp_fn,"Make Binary","")
-#IJ.run(imp_fn,"Invert","")
-imp_fn.show()
+IJ.run(imp_nuc,"Set Scale...", "distance=1 known=1 pixel=1 unit=pixels")
+IJ.run(imp_nuc,"Gaussian Blur...","sigma=5")
+IJ.run(imp_nuc,"Make Binary","")
 
-# Create a table to store the results
-rt = ResultsTable()
-paOpts = PA.SHOW_OUTLINES \
-        + PA.INCLUDE_HOLES \
-        + PA.EXCLUDE_EDGE_PARTICLES
-measurements = PA.CENTROID + PA.AREA
-MINSIZE = 0
-MAXSIZE = Double.POSITIVE_INFINITY
-pa = PA(paOpts,measurements, rt, MINSIZE, MAXSIZE)
-pa.setHideOutputImage(True)
- 
-if pa.analyze(imp_fn):
-    print "All ok"
-else:
-    print "There was a problem in analyzing", imp_fn
+def getParticleCenters(imp):
+    # Create a table to store the results
+    rt = ResultsTable()
+    paOpts = PA.SHOW_OUTLINES \
+            + PA.INCLUDE_HOLES \
+            + PA.EXCLUDE_EDGE_PARTICLES
+    measurements = PA.CENTROID + PA.CENTER_OF_MASS
+    MINSIZE = 0
+    MAXSIZE = Double.POSITIVE_INFINITY
+    pa = PA(paOpts,measurements, rt, MINSIZE, MAXSIZE)
+    pa.setHideOutputImage(True)
+     
+    if not pa.analyze(imp):
+        print "There was a problem in analyzing", imp
 
-# The measured centroids are listed in the first column of the results table, as a float array:
-centroids_x = rt.getColumn(rt.X_CENTROID)
-centroids_y = rt.getColumn(rt.Y_CENTROID)
+    # The measured centroids are listed in the first column of the results table, as a float array:
+    centroids_x = rt.getColumn(rt.X_CENTROID)
+    centroids_y = rt.getColumn(rt.Y_CENTROID)
+    coms_x = rt.getColumn(rt.X_CENTER_OF_MASS)
+    coms_y = rt.getColumn(rt.Y_CENTER_OF_MASS)
 
-cx = int(round(centroids_x[0]))
-cy = int(round(centroids_y[0]))
+    return (centroids_x,centroids_y, coms_x, coms_y)
 
-fn_centroid_roi = PointRoi(cx,cy)
+# centroid of fibronectin anchor
+centers = getParticleCenters(imp_fn)
+cxfn = int(round(centers[0][0]))
+cyfn = int(round(centers[1][0]))
+fn_centroid_roi = PointRoi(cxfn,cyfn)
 fn_centroid_roi.setDefaultMarkerSize("Large")
+fn_centroid_roi.setStrokeColor(Color.CYAN)
+
+# center of mass of nucleus 
+centers = getParticleCenters(imp_nuc)
+cxnuc = int(round(centers[2][0]))
+cynuc = int(round(centers[3][0]))
+nuc_com_roi = PointRoi(cxnuc,cynuc)
+nuc_com_roi.setDefaultMarkerSize("Large")
+
+# create composite
+print "creating composite"
+comp = ImagePlus("composite",imp_nuc_orig.getProcessor().convertToColorProcessor())
+comp.getProcessor().setChannel(2,imp_fn_orig.getProcessor())
+comp.show()
+comp.getProcessor().drawRoi(fn_centroid_roi)
+comp.getProcessor().drawRoi(nuc_com_roi)
+comp.repaintWindow()
+IJ.saveAsTiff(comp,"/output/" + "comp_jaakko1.tif")
 
 IJ.run(imp_fn,"Skeletonize","")
-#imp_fn.getProcessor().drawRoi(fn_centroid_roi)
-imp_fn.repaintWindow()
 #wait = Wait("msg!")
 #wait.show()
 
