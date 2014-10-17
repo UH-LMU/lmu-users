a97e9f3bb2425cbfe5fcf746879280d7f22ed6f2
Harri Jäälinoja <harri.jaalinoja at helsinki.fi>
2014-10-17 15:02:58 +0000

    add nucleus center of mass, corrected distances

--- jaakko/draw_centers.py
+++ jaakko/draw_centers.py
@@ -17,15 +17,17 @@ def draw_centers(data):
 
     nx = int(data[4])
     ny = int(data[5])
-    ax = int(data[6])
-    ay = int(data[7])
+    nx_com = int(data[6])
+    ny_com = int(data[7])
+    ax = int(data[8])
+    ay = int(data[9])
     
-    orientation = 2*math.pi * float(data[8]) / 360.0
+    orientation = 2*math.pi * float(data[10]) / 360.0
     line_dy = int(round(math.atan(orientation) * LINE_DX))
     ax2 = ax - LINE_DX
     ay2 = ay - line_dy
 
-    orientation_avg = 2*math.pi * float(data[9]) / 360.0
+    orientation_avg = 2*math.pi * float(data[11]) / 360.0
     line_dy = int(round(math.atan(orientation_avg) * LINE_DX))
     ax3 = ax - LINE_DX
     ay3 = ay - line_dy
@@ -35,10 +37,14 @@ def draw_centers(data):
     roi_nucleus = PointRoi(nx,ny)
     roi_nucleus.setDefaultMarkerSize("Large")
     roi_nucleus.setStrokeColor(Color.CYAN)
+    roi_nucleus_com = PointRoi(nx,ny)
+    roi_nucleus_com.setDefaultMarkerSize("Large")
+    roi_nucleus_com.setStrokeColor(Color.GREEN)
     roi_anchor = PointRoi(ax,ay)
     roi_anchor.setDefaultMarkerSize("Large")
  
     imp.getProcessor().drawRoi(roi_nucleus)
+    imp.getProcessor().drawRoi(roi_nucleus_com)
     imp.getProcessor().drawRoi(roi_anchor)
 
     imp.setColor(Color.RED)
--- jaakko/readdb.py
+++ jaakko/readdb.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy.sql import func
 from sqlalchemy.ext.automap import automap_base
 
+import math
 import os
 import sys
 
@@ -34,17 +35,13 @@ if __name__ == '__main__':
             image.Image_Metadata_Plate, \
             nucleus.nucleus_AreaShape_Center_X,\
             nucleus.nucleus_AreaShape_Center_Y, \
+            nucleus.nucleus_Location_Center_X,\
+            nucleus.nucleus_Location_Center_Y, \
             anchor.anchor_AreaShape_Center_X,\
             anchor.anchor_AreaShape_Center_Y, \
             anchor.anchor_AreaShape_Orientation]
 
  
-    query = session.query(*columns) \
-            .join(nucleus,image.ImageNumber==nucleus.ImageNumber) \
-            .join(anchor,image.ImageNumber==anchor.ImageNumber)
-
-    results = query.all()
-
     # find unique plates and average orientation per plate
     plates = session.query(image.Image_Metadata_Plate).distinct()
     avgs = {}
@@ -57,7 +54,66 @@ if __name__ == '__main__':
         #print p[0], avg[0]
         avgs[p[0]] = avg[0]
 
+    # print header row
+    #print "'%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s'" % \
 
-    for r in results:
-        print "%d,'%s','%s','%s',%d,%d,%d,%d,%f,%f" % (tuple(r) + (avgs[r[3]],))
+    headers = ("image_number","overlay_path","overlay_file","plate", \
+            "nucleus_x","nucleus_y","nucleus_com_x","nucleus_com_y",\
+            "anchor_x","anchor_y","anchor_angle","anchor_angle_average", \
+            "dx_shape","dx_shape_corrected","dx_com","dx_com_corrected")
+    fmt = ""
+    for h in headers:
+        fmt = fmt + "'%s',"
+
+    print fmt % headers
+
+    # find results for images that were not discarded
+    query = session.query(*columns) \
+            .join(nucleus,image.ImageNumber==nucleus.ImageNumber) \
+            .join(anchor,image.ImageNumber==anchor.ImageNumber) \
+            .filter(image.Image_Metadata_QCFlag_multiple_anchors==0) \
+            .filter(image.Image_Metadata_QCFlag_no_nucleus==0)
+
+    for r in query.all():
+        # make a tuple of results
+        tr = tuple(r)
+
+        # average anchor orientation for this plate
+        ao = avgs[r[3]]
+
+        # distance between nucleus and anchor x-coordinates
+        dx = r[8] - r[4]
+
+        # distance between nucleus and anchor center of mass x-coordinates
+        dx_com = r[8] - r[6]
+
+        # distances corrected by average anchor orientation
+        dx_corr = dx / math.cos(2 * math.pi * ao / 360.0)
+        dx_com_corr = dx_com / math.cos(2 * math.pi * ao / 360.0)
+
+        tr = tr + (ao,dx,dx_corr,dx_com,dx_com_corr)
+
+        print "%d,'%s','%s','%s',%d,%d,%d,%d,%d,%d,%f,%f,%d,%f,%d,%f" % tr
+
+
+    # print empty line
+    print ",,,,,,,,,,,,,,,"
+
+    # list discarded images
+    query = session.query(*columns) \
+            .join(nucleus,image.ImageNumber==nucleus.ImageNumber) \
+            .join(anchor,image.ImageNumber==anchor.ImageNumber) \
+            .filter(image.Image_Metadata_QCFlag_multiple_anchors==1)
+    for r in query.all():
+        print "%d,'%s','%s','%s',%d,%d,%d,%d,%d,%d,%f,%s,,,," % (tuple(r) + ("bad anchor",))
+
+    query = session.query(*columns) \
+            .join(nucleus,image.ImageNumber==nucleus.ImageNumber) \
+            .join(anchor,image.ImageNumber==anchor.ImageNumber) \
+            .filter(image.Image_Metadata_QCFlag_no_nucleus==1)
+    for r in query.all():
+        print "%d,'%s','%s','%s',%d,%d,%d,%d,%d,%d,%f,%s,,,," % (tuple(r) + ("no nucleus",))
+
+
+
 
